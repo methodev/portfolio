e4cd22f4e8f933c2a2f462bc4192c24be8029dd9
Martin Metodiev <metodiev.martin@gmail.com>
2017-03-28 18:29:15 +0300

    [SYS] Rename "conventions" directory to "linters"

--- configs/grunt/puglint.js
+++ configs/grunt/puglint.js
@@ -2,7 +2,7 @@ module.exports = {
   task: {
     options: {
       config: {
-        'extends': './configs/conventions/.pug-lintrc'
+        'extends': './configs/linters/.pug-lintrc'
       }
     },
     src: ['src/markup/**/*.pug']
